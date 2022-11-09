efcc2fbdf6bde7fc88303b2c57cdfb3daa76f2d3
nurliaaa <nur.lia.tif20@polban.ac.id>
2022-11-10 01:36:25 +0700

    CI project

---
+++ app/Views/pages/v_main.php
@@ -0,0 +1,40 @@
+<div id="myCarousel" class="carousel slide banner_main" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <div class="container-fluid">
+                <div class="carousel-caption">
+                    <div class="row">
+                        <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12">
+                            <div class="text-bg">
+                                <img src="<?= base_url('images/img/Group 12.png') ?>" alt="#" style="width:50%;max-width:200px" />
+                                <h1>Titipkan<br>Helm Anda</h1>
+                                <p>Tenangkan Hati Anda Dengan Menitipkan Barang Anda </p>
+                                <!-- <div class="col-xl-3 col-md-6" data-aos="fade-up" data-aos-delay="200">
+                                    <div class="carousel-icon-box">
+                                        <div class="icon"><i class="bi bi-gem"></i></div>
+                                        <h4 class="title"><a href="/c_mahasiswa">Mahasiswa</a></h4>
+                                    </div>
+                                </div> -->
+                                <a class="read_more" href="/pageLoker">Ambil</a>
+                                <a class="read_more" href="/pageLoker">Simpan</a>
+                            </div>
+                        </div>
+                        <div class="padding_left100 col-xl-20 col-lg-5 col-md-10 col-sm-10">
+                            <img src="images/img/Layer.png">
+                            <div class="images_box">
+                                <figure></figure>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+        <i class="fa fa-long-arrow-left" aria-hidden="true"></i>
+    </a>
+    <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+        <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+    </a>
+</div>
---
+++ app/Views/pages/v_simpan.php
@@ -0,0 +1,20 @@
+    <div id="work" class="work">
+       <div class="container-fluid">
+          <div class="row d_flex">
+             <div class="col-md-7">
+                <div class="titlepage">
+                   <center>
+                      <h1>Berhasil Tersimpan</h1>
+                      </br>
+                      <a class="read_more" href="/">Kembali</a>
+                   </center>
+                </div>
+             </div>
+             <div class="col-md-5">
+                <div class="work_img">
+                   <figure><img src="images/img/simpan.jpg" alt="#"></figure>
+                </div>
+             </div>
+          </div>
+       </div>
+    </div>
